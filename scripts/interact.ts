3c0f805ab84cd18ef34c976da322987125b40ddb
Isaac <teryima127@gmail.com>
2023-09-11 00:15:20 +0100

    changes

--- scripts/interact.ts
+++ scripts/interact.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
 
 async function main() {
@@ -11,6 +11,29 @@ async function main() {
     let contractA = await ethers.getContractAt("ITokenA", tokenA)
     let contractB = await ethers.getContractAt("ITokenB", tokenB)
     const swap = await ethers.getContractAt("ITokenSwap", tokenSwap)
+
+    const unlimitedApprovalTokenA = ethers.parseEther("100");
+    const unlimitedApprovalTokenB = ethers.parseEther("300")
+
+    const impersonatedSigner = await ethers.getImpersonatedSigner(signer)
+
+    //@ts-ignore
+    await network.provider.send('hardhat_setBalance', [
+        signer,
+        '0x6342FD14ED9ACF95F0000',
+    ])
+
+    // approve contractA and ContractB to spend tokens
+    await contractA.connect(impersonatedSigner).approve(swap, unlimitedApprovalTokenA)
+    await contractB.connect(impersonatedSigner).approve(swap, unlimitedApprovalTokenB)
+
+
+    const addTokenA = ethers.parseUnits("70")
+    const addtokenB = ethers.parseUnits("100")
+
+    
+
+
     
  }
 
