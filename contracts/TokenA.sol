93142f4b40ef00dfaff01154b7a5a14f30c429ef
Isaac <teryima127@gmail.com>
2023-09-10 17:41:31 +0100

    created and deployed token A

--- contracts/TokenA.sol
+++ contracts/TokenA.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract MyToken is ERC20 {
+contract TokenA is ERC20 {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
         // Mint 100 tokens to msg.sender
         _mint(msg.sender, 100 * 10 ** uint(decimals()));
