f727488da77aea7928ab323e02bb736aa659e0d9
Isaac <teryima127@gmail.com>
2023-09-10 18:54:32 +0100

    tokenswap script

--- contracts/TokwenSwap.sol
+++ contracts/TokwenSwap.sol
@@ -1,4 +1,5 @@
-pragma solidity >=0.5.19;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
---
+++ scripts/TokenSwap.ts
@@ -0,0 +1,23 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const tokenA = "0x8178831cca0835807f3C9D020776d0a20f28e8b2"
+    const tokenB = "0x2c5ac3e6897A02F40bCbBD95aBE949d76ed94748"
+    
+  const tokenSwap = await ethers.deployContract("TokenSwap", [tokenA, tokenB], {
+  });
+
+  await tokenSwap.waitForDeployment();
+
+  console.log(`Deployed at ${tokenSwap.target}`)
+
+
+ }
+
+ main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+
+// Tokenswap was deployed at: 0x4f74F84a027D9C93A88d8916B9CE78f0Ae057411
